05c84e06f648d508fef1c6ee88c5555ce3295fdb
Micah <micahculpepper@gmail.com>
2016-09-30 07:58:59 -0500

    backup

--- flaskplayground/__main__.py
+++ flaskplayground/__main__.py
@@ -5,14 +5,35 @@
 Main module of flaskplayground. Provides command-line entry point and main logic.
 """
 
+from flask import Flask
+from flask import request
+app = Flask(__name__)
 
-def main(args=None):
-    """CLI entry point"""
-    if not args:
-        import parsers
-        parser = parsers.main()
-        args = parser.parse_args()
+
+@app.route('/')
+def hello_world():
+    return "Hello, World!"
+
+
+@app.route('/report/')
+def report():
+    with open('data/data.csv') as f:
+        rows = f.readlines()
+    html_table_rows = []
+    for row in rows:
+        html_table_rows.append(
+            "<tr><td>" + "</td><td>".join([cell for cell in row.split(",")]) + "</td></tr>"
+            )
+    return "\n".join(html_table_rows)
+
+
+# def main(args=None):
+#     """CLI entry point"""
+#     if not args:
+#         import parsers
+#         parser = parsers.main()
+#         args = parser.parse_args()
 
 
 if __name__ == '__main__':
-    main()
+    app.run(host='127.0.0.1', debug=True)  # ssl_context='adhoc'
---
+++ flaskplayground/tests/tests.py
@@ -0,0 +1,29 @@
+# -*- coding: UTF-8 -*-
+
+"""
+Unit tests for flaskplayground
+"""
+
+import unittest
+import flaskplayground.__main__
+import flaskplayground.parsers
+
+
+# class TestMain(unittest.TestCase):
+#     """Test the __main__ module"""
+#     def setUp(self):
+#         self.parser = {0}.parsers.main()
+#
+#     def test_cli(self):
+#         cli_args = ["--foo"]
+#         args = self.parser.parse_args(cli_args)
+#         {0}.__main__.main(args)
+
+# Testing Flask
+# from flask import request
+#
+# with app.test_request_context('/hello', method='POST'):
+#     # now you can do something with the request until the
+#     # end of the with block, such as basic assertions:
+#     assert request.path == '/hello'
+#     assert request.method == 'POST'
